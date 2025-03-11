c3b78e720b5e4067011ff0c40deb33da287e8a57
brucebianchi <brucebianchini@gmail.com>
2025-03-11 08:37:21 -0300

    Update reports.py

--- reports.py
+++ reports.py
@@ -9,7 +9,6 @@ def gerar_relatorio_vendas(start_date, end_date, obter_vendas_anselmo, obter_ven
     while current_date <= end_date:
         data_formatada = current_date.strftime('%d/%m/%Y')
         
-        # Passando as credenciais corretamente para as funções de obtenção de vendas
         dados_anselmo = obter_vendas_anselmo(data_formatada, data_formatada, app_key_anselmo, app_secret_anselmo)
         vendas_anselmo = dados_anselmo['pedidoVenda']['vFaturadas'] if dados_anselmo else 0
         
@@ -30,3 +29,28 @@ def gerar_relatorio_vendas(start_date, end_date, obter_vendas_anselmo, obter_ven
         current_date += timedelta(days=1)
     
     return pd.DataFrame(vendas_data)
+
+def gerar_relatorio_vendedores(start_date, end_date, obter_vendedores_unicos_e_vendas_anselmo, obter_vendedores_unicos_e_vendas_favinco, app_key_anselmo, app_secret_anselmo, app_key_favinco, app_secret_favinco):
+    vendedores_info = []
+    
+    # Obter os vendedores únicos e suas vendas totais para Anselmo
+    vendedores_unicos_anselmo = obter_vendedores_unicos_e_vendas_anselmo(start_date.strftime('%d/%m/%Y'), end_date.strftime('%d/%m/%Y'), app_key_anselmo, app_secret_anselmo)
+    
+    # Obter os vendedores únicos e suas vendas totais para Favinco
+    vendedores_unicos_favinco = obter_vendedores_unicos_e_vendas_favinco(start_date.strftime('%d/%m/%Y'), end_date.strftime('%d/%m/%Y'), app_key_favinco, app_secret_favinco)
+    
+    # Unir os dados dos vendedores e somar as vendas
+    for vendedor_id in set(vendedores_unicos_anselmo.keys()).union(vendedores_unicos_favinco.keys()):
+        total_vendas_anselmo = vendedores_unicos_anselmo.get(vendedor_id, 0)
+        total_vendas_favinco = vendedores_unicos_favinco.get(vendedor_id, 0)
+        
+        vendedores_info.append({
+            'Vendedor': vendedor_id,
+            'Vendas Anselmo': f"R$ {total_vendas_anselmo:,.2f}",
+            'Vendas Favinco': f"R$ {total_vendas_favinco:,.2f}",
+            'Total de Vendas': f"R$ {total_vendas_anselmo + total_vendas_favinco:,.2f}"
+        })
+    
+    # Criar DataFrame com os dados dos vendedores
+    df_vendedores = pd.DataFrame(vendedores_info)
+    return df_vendedores
