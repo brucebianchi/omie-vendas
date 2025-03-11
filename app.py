d9b95555d9ed483d95600dcb211a0d651b4ed8c4
brucebianchi <brucebianchini@gmail.com>
2025-03-11 16:53:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import streamlit as st
 from datetime import datetime
 from api import obter_vendas_anselmo, obter_vendas_favinco
 from relatorio import gerar_relatorio_vendas, gerar_relatorio_vendedores
@@ -24,10 +23,17 @@ if start_date > end_date:
     st.error("A data de início não pode ser maior que a data de fim.")
 else:
     if st.button('Gerar Relatório'):
-        df_vendas = gerar_relatorio_vendas(start_date, end_date, obter_vendas_anselmo, obter_vendas_favinco)
+        # Passar as chaves de API para a função
+        df_vendas = gerar_relatorio_vendas(start_date, end_date, 
+                                           lambda data_inicial, data_final: obter_vendas_anselmo(data_inicial, data_final, app_key_anselmo, app_secret_anselmo),
+                                           lambda data_inicial, data_final: obter_vendas_favinco(data_inicial, data_final, app_key_favinco, app_secret_favinco))
+        
         st.markdown("<h3 style='color:orange;'>Relatório de Vendas Diárias</h3>", unsafe_allow_html=True)
         st.write(df_vendas)
         
-        df_vendedores = gerar_relatorio_vendedores(start_date, end_date, obter_vendedores_unicos_e_vendas_anselmo, obter_vendedores_unicos_e_vendas_favinco)
+        df_vendedores = gerar_relatorio_vendedores(start_date, end_date, 
+                                                   lambda data_inicial, data_final: obter_vendedores_unicos_e_vendas_anselmo(data_inicial, data_final, app_key_anselmo, app_secret_anselmo),
+                                                   lambda data_inicial, data_final: obter_vendedores_unicos_e_vendas_favinco(data_inicial, data_final, app_key_favinco, app_secret_favinco))
+        
         st.markdown("<h3 style='color:green;'>Total de Vendas por Vendedor</h3>", unsafe_allow_html=True)
         st.write(df_vendedores)
